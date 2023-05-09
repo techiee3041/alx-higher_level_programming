1239f26786e9285c7af739e63dd30a899e139f62
techiee3041 <doreenoms6699@gmail.com>
2023-05-09 17:50:43 +0300

    removed an ectra print function

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02d}".format(i), end='')
     if i < 99:
-        print(", ", end='')
-print()
+        print("{:02d}".format(i), end=', ')
+    elif i == 99:
+        print("{}".format(i))
