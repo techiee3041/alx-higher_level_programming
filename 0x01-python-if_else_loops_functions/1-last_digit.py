a2f441df2389e9947cb03f99a9b94d8648e96552
techiee3041 <doreenoms6699@gmail.com>
2023-05-09 16:22:02 +0300

    python script to check whether last digit is positive

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_num = abs(number)%10
+if number < 0:
+    last_num = -last_num
+print(f"Last digit of {number} is {last_num} and is ".format(number, last_num), end = "")
+if last_num > 5:
+    print("greater than 5")
+elif last_num == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
