15a627d5ba364c51417bdb32d5efa33d59184c6d
techiee3041 <doreenoms6699@gmail.com>
2023-05-11 18:38:09 +0300

    changed to meet requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Write a program that prints the number of and the list of its arguments"""
-import sys
+if __name__ == "__main__":
+    """prints the number of and the list of its arguments"""
+    import sys
 
-x = len(sys.argv) - 1
-if x == 0:
-    print(f"{x} arguments.")
-elif x == 1:
-    print(f"{x} argument:")
-else:
-    print(f"{x} arguments:")
+    x = len(sys.argv) - 1
+    if x == 0:
+        print(f"{x} arguments.")
+    elif x == 1:
+        print(f"{x} argument:")
+    else:
+        print(f"{x} arguments:")
 
-for i in range(x):
-    print(f"{i + 1}: {sys.argv[i + 1]}")
+    for i in range(x):
+        print(f"{i + 1}: {sys.argv[i + 1]}")
