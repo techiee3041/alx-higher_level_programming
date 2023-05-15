2756035597f67a5978cfb8b2b5d6a27d3148f0e1
techiee3041 <doreenoms6699@gmail.com>
2023-05-15 12:11:47 +0300

    correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is "":
-        return(0, None)
+        return None
     else:
         length = len(sentence)
         first = sentence[0]
