60b0ad4b9095fd1f0241032f0a5ec3f0e9c085b2
techiee3041 <doreenoms6699@gmail.com>
2023-05-29 22:32:11 +0300

    solution

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """function that raises a name error
+    Args:
+        message: message to be printed after raising the error
+    Returns:
+        None
+    """
+    raise NameError(message)
