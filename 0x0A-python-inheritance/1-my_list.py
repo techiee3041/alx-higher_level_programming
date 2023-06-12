1fddb48392b115515bf8b5d5e61c12496c6655ab
techiee3041 <doreenoms6699@gmail.com>
2023-06-12 22:09:18 +0300

    solution

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+    """
+
+    def __init__(self):
+        """initializes the object"""
+
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+
+        This method sorts the list in ascending order
+        using the built-in sorted function,
+        and then prints the sorted list to the console.
+
+        """
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+# 0x0A. Python - Inheritance
+
+in this project, I learnt about Python Inheritance. i got to understand the benefits of inheritance in python as well as
+the differences between superclasses and sub-classes
