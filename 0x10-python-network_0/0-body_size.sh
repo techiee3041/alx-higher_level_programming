59e03b05a4e8efe0cb5c13d0f38948836162f0e7
techiee3041 <doreenoms6699@gmail.com>
2023-08-31 22:21:51 +0300

    submit solution

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# | = Pipe the output of the previous command to the next command.4
-# wc -c = Print the byte size of the HTTP response header.(character count)
+#takes in a URL, sends a request to that URL,and displays the size of the body of the response
 curl -s "$1" | wc -c
